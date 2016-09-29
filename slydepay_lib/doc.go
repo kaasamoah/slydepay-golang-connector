53373bf6756a5e97d37c0fe428e43a2c6f5260c3
Kofi Asamoah <yoofi@dreamoval.com>
2016-09-29 10:54:42 +0000

    Pushing local updates

--- slydepay_lib/doc.go
+++
@@ -1,6 +0,0 @@
-// slydepay_lib project doc.go
-
-/*
-slydepay_lib document
-*/
-package slydepay_lib
--- slydepay_lib/model/lineitem.go
+++
@@ -1,9 +0,0 @@
-package model
-
-type LineItem struct {
-	ItemCode  string
-	ItemName  string
-	Quantity  int16
-	UnitPrice float64
-	SubTotal  float64
-}
